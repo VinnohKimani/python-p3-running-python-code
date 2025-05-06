d8e9c08e4829b7345498a9c7244030c533cca1f8
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-05-06 14:09:18 +0300

    Lab complete

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+
+
+print("Hello World! Pass this test, please.")
+
+
+# def test_prints_hello_world(self):
+#          self="Hello World! Pass this test, please."
+
+#          print(test_prints_hello_world)
+#          print(self)
+
+    
